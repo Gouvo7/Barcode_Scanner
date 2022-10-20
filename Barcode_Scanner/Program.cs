cfcbcb7c0a2f9cf9a167164615388afcb225a6e3
Nektarios Gouvousis <gouvousisnektarios@gmail.com>
2022-10-20 16:52:49 +0300

    Double-Checking does not work properly.

--- Barcode_Scanner/Program.cs
+++ Barcode_Scanner/Program.cs
@@ -6,6 +6,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
 using System.IO;
+using System.Threading;
+
 
 namespace Barcode_Scanner
 {
@@ -14,9 +16,8 @@ namespace Barcode_Scanner
 
         // Crops the image to a fixed location where the barcode is
         [Obsolete]
-        public string Crop_Img(string path,string png_path)
+        public string Crop_Img(string png_path)
         {
-
             try
             {
                 Image img = Image.FromFile(png_path);
@@ -28,7 +29,6 @@ namespace Barcode_Scanner
                 img.Dispose();
                 bmpImage.Dispose();
                 return barcode;
-
             }
             catch (Exception e)
             {
@@ -36,46 +36,39 @@ namespace Barcode_Scanner
             }
         }
 
-        // Scans the barcode number
+        /* Receives the name of the png file, scans the barcode number and returns it into a string */
         [Obsolete]
-        public string Scan_Barcode(string cropped)
+        public string Scan_Barcode(string cropped_png)
         {
-            BarcodeResult Result = BarcodeReader.QuicklyReadOneBarcode(cropped);
-            BarcodeReader.QuicklyReadOneBarcode("a.png");
-            if (Result != null)
+            BarcodeResult res = BarcodeReader.QuicklyReadOneBarcode(cropped_png);
+            if (res != null)
             {
-                Console.WriteLine("Barcode read with no errors! Barcode is:" + Result.Text+ "\n\n");
+                Console.WriteLine("Barcode read with no errors! Barcode is:" + res.Text+ "\n");
+                return res.Text;
             }
             else
             {
-                Console.WriteLine("Could not identify barcode\n\n");
-            }
-            if (Result!= null)
-                return Result.Text;
-            else
                 return null;
+            }
         }
 
-        // Deletes the temporary files
+        /* Deletes the temporary files */
         public void File_Deleter(string pdf_file, string png_path)
         {
-            //try
-            //{
-                //File.Delete(pdf_file);        // If you want to delete the PDF afterwards, uncomment this line.
-            if (File.Exists(png_path))
+            // If you want to delete the PDF afterwards, uncomment this line.
+            try
             {
-                File.Delete(png_path);
-                
+                if (File.Exists(png_path))
+                    File.Delete(png_path);
             }
-            Console.WriteLine(png_path);
-
-            //File.Delete(cropped);
-            //}
-            //catch (System.IO.IOException) { 
-            //}
+            catch (System.IO.IOException)
+            {
+                throw new IOException("Something went wrong with the deletion of temporary files");
+            }
         }
 
-        // Converts the first page of the PDF file to a PNG.
+        // Converts the first page of the PDF file to a PNG and returns the number of pages
+        // (only for database appendance).
         public int Create_Png(string pdf_path, string pdf_file)
         {
             PdfDocument pdf = new PdfDocument(pdf_path + pdf_file);
@@ -88,7 +81,6 @@ namespace Barcode_Scanner
             pdf.Pages[0].Save(new_path, options);
             int pages = pdf.PageCount;
             pdf.Dispose();
-            Console.WriteLine("Number of pages for each document: " + pages);
             return pages;
         }
 
@@ -98,69 +90,96 @@ namespace Barcode_Scanner
             Stopwatch timer = new Stopwatch();
             timer.Start();
             List<List<string>> mylist = new List<List<string>>();
-            
+
             Program p = new Program();
             string pdf_path = "C:\\Users\\ngouvousis\\Desktop\\PDFS\\";  // Change if your pdfs are in another directory
-            string png_path = "";
-            string pdf_file = null;
-            string[] dirs = Directory.GetFiles(pdf_path, "*.pdf", SearchOption.AllDirectories);
-            
-            foreach (string dir in dirs)
+            string png_path, pdf_file;
+            try
             {
-                //Console.WriteLine(dir); // dir is the whole path
-                pdf_file = Path.GetFileName(dir); // filename is the name of the file
-                string f = pdf_file.Remove(pdf_file.Length - 3); // f is the file name without the extension
-                int pages = p.Create_Png(pdf_path, pdf_file);
-                string png_f = f + "png";
-                png_path = pdf_path + png_f;
-                Console.WriteLine("PNG_Path is : " + png_path);
-                string barc = p.Crop_Img(pdf_path, png_path);
-                if (barc!=null && barc!=" ") { 
-                    mylist.Add(new List<string> { barc, pdf_file, pages.ToString() });
-                    
+                string[] dirs = Directory.GetFiles(pdf_path, "*.pdf", SearchOption.AllDirectories);
+                Console.WriteLine("Number of PDF files found in the path '" + pdf_path + "' is: " + dirs.Length);
+                
+                foreach (string dir in dirs) // For each file found in the directory given
+                {
+                    pdf_file = Path.GetFileName(dir); // filename is the name of the file
+                    string f = pdf_file.Remove(pdf_file.Length - 3); // f is the file name without the extension
+                    int pages = p.Create_Png(pdf_path, pdf_file); // Converts the pdf into an image
+                    string png_f = f + "png";                     
+                    png_path = pdf_path + png_f;
+                    string barc = p.Crop_Img(png_path);
+                    if (barc != null && barc != " ")
+                        mylist.Add(new List<string> { barc, pdf_file, pages.ToString() });
+                    p.File_Deleter(pdf_file, png_path);
                 }
-                p.File_Deleter(pdf_file, png_path);
             }
-            string connectionString = "server=localhost;uid=root;pwd=root;database=lawdb";
-            //myConnectionString = "server=10.1.11.28:3306;uid=ngouvousis;pwd=Nek@niro_{Gou22};database=lawdb";
-            int i = 0;
+            catch (System.IO.DirectoryNotFoundException)
+            {
+                Console.Write("Could not find any PDF files in the path that you have given. Program will now exit");
+                Thread.Sleep(750);
+                Console.Write(".");
+                Thread.Sleep(750);
+                Console.Write(".");
+                Thread.Sleep(750);
+                Console.Write(".");
+                Thread.Sleep(1000);
+            }
+
             string tmp = "Test Eggrafo";
             DateTime now = DateTime.Now;
+            //myConnectionString = "server=10.1.11.28:3306;uid=ngouvousis;pwd=Nek@niro_{Gou22};database=lawdb";
+            string connectionString = "server=localhost;uid=root;pwd=root;database=lawdb";
 
+            /* For each successfully scanned barcode, we will insert the corresponding values 
+             * to the table */
             foreach (List<String> sublist in mylist)
             {
-                i++;
+                bool canInsert = false;
                 using (MySqlConnection conn = new MySqlConnection(connectionString))
                 {
-                    //MySql.Data.MySqlClient.MySqlCommand query = new MySql.Data.MySqlClient.MySqlCommand("SELECT * FROM WCM_LAC where WCMLAC_ID = '"+x+"'" , conn);
-                    //string query = "INSERT INTO WCM_DOC VALUES (NULL,@pr_id,wcm_lac,wcmLAC_ID,@wcnDoc_Title,@wcmDoc_Type,wcmDOC_FileType,@wcmDoc_URL,wcmDOC_Pages,@create_dt,@last_upd,@wdc_lock,0,NULL,NULL";
-                    string query_tmp = "INSERT INTO WCM_DOC VALUES (NULL, @pr_id, 'wcm_lac', 'wcmLAC_ID', '"+ tmp +"', 200, 9, @wcmDoc_URL, @wcmDoc_Pages, @create_dt, @last_upd, 0, 0, NULL, NULL)";
-
+                    string query_tmp = "select * from wcm_doc where wcmdoc_URL like '%"+"@url"+"%'";
                     using (MySqlCommand command = new MySqlCommand(query_tmp, conn))
                     {
-                        command.Parameters.AddWithValue("@pr_id",sublist[0] );
-                        command.Parameters.AddWithValue("@wcmDoc_URL", sublist[1]);
-                        command.Parameters.AddWithValue("@wcmDOC_Pages", sublist[2]);
-                        command.Parameters.AddWithValue("@create_dt", now);
-                        command.Parameters.AddWithValue("@last_upd", now);
+                        command.Parameters.AddWithValue("@url", sublist[0]);
                         conn.Open();
-                        try
-                        {
-                            int res = command.ExecuteNonQuery();
-                            if (res < 0)
-                                Console.WriteLine("There was an error during data insertion.");
-                            else
-                                Console.WriteLine("Very good very nice!");
-                        }
-                        catch (MySqlException){
-                            Console.WriteLine("Error: Duplicate entry was found for doc: " + sublist[1]);}
+                            using (MySqlDataReader reader = command.ExecuteReader())
+                            {
+                                if (reader.Read())
+                                    Console.WriteLine("CANNOT INSERT");
+                                else
+                                    canInsert = true;
+                            }
                     }
                 }
-            }            
+                if (canInsert)
+                {
+                    using (MySqlConnection conn = new MySqlConnection(connectionString))
+                    {
+                        string query_tmp = "INSERT INTO WCM_DOC VALUES (NULL, @pr_id, 'wcm_lac', 'wcmLAC_ID', '" + tmp + "', " +
+                            "200, 9, @wcmDoc_URL, @wcmDoc_Pages, @create_dt, @last_upd, 0, 0, NULL, NULL)";
+                        using (MySqlCommand command = new MySqlCommand(query_tmp, conn))
+                        {
+                            command.Parameters.AddWithValue("@pr_id", sublist[0]);
+                            command.Parameters.AddWithValue("@wcmDoc_URL", sublist[1]);
+                            command.Parameters.AddWithValue("@wcmDOC_Pages", sublist[2]);
+                            command.Parameters.AddWithValue("@create_dt", now);
+                            command.Parameters.AddWithValue("@last_upd", now);
+                            conn.Open();
+                            try
+                            {
+                                int res = command.ExecuteNonQuery();
+                                if (res < 0)
+                                    Console.WriteLine("There was an error during data insertion.");
+                            }
+                            catch (MySqlException)
+                            {
+                                Console.WriteLine("Error: Duplicate entry was found for doc: " + sublist[1]);
+                            }
+                        }
+                    }
+                }
+            }
             timer.Stop();
             Console.WriteLine("time taken = " + timer.Elapsed);
         }
-
-        
     }
 }
